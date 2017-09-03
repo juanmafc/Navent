a5e42a2259aac88c7678f79c1c8cf4850c756a32
juanmafc <juanmafc25@gmail.com>
2017-09-03 14:51:50 -0300

    Estructura para calcular el promedio de una imagen

---
+++ image_average.py
@@ -0,0 +1,20 @@
+import Image
+
+def calcularPromedio((x0,y0), ancho, alto, imagen):
+    r, g, b = 0, 0, 0
+    cantidad = 0
+    for x in range(x0, x0+ancho):
+        for y in range(y0, y0+alto):
+            pixelR, pixelG, pixelB = imagen.getpixel((x,y))
+            r += pixelR
+            g += pixelG
+            b += pixelB
+            cantidad += 1
+    return ((r/cantidad), (g/cantidad), (b/cantidad))
+
+
+
+imagen = Image.open('test.png').convert('RGB')
+ancho, alto = imagen.size
+r, g, b = calcularPromedio((0,0), ancho, alto, imagen)
+print r,g,b
