0379648d5f7a2d147a0a4b30286ad9562411e6fe
juanmafc <juanmafc25@gmail.com>
2017-09-04 00:18:06 -0300

    Se chequea que el x0, y0 esten dentro de los limites de la imagen

--- image_average.py
+++ image_average.py
@@ -4,13 +4,15 @@ import os
 def calcularPromedio((x0,y0), ancho, alto, imagen):
     r, g, b = 0, 0, 0
     cantidad = 0
+    anchoImagen, altoImagen = imagen.size
     for x in range(x0, x0+ancho):
         for y in range(y0, y0+alto):
-            pixelR, pixelG, pixelB = imagen.getpixel((x,y))
-            r += pixelR
-            g += pixelG
-            b += pixelB
-            cantidad += 1
+            if ( (x < anchoImagen) and (y < altoImagen) ):
+                pixelR, pixelG, pixelB = imagen.getpixel((x,y))
+                r += pixelR
+                g += pixelG
+                b += pixelB
+                cantidad += 1
     return [(r/cantidad), (g/cantidad), (b/cantidad)]
 
 
@@ -18,15 +20,15 @@ def procesarPromedioDeImagen(imagen):
     ancho, alto = imagen.size
     promedios = []
 
-    stepHorizontal = ancho/2
-    stepVertical = alto/2
+    stepHorizontal = ancho/4
+    stepVertical = alto/4
     for offsetHorizontal in range(0, ancho - 1, stepHorizontal):
         for offsetVertical in range(0, alto - 1, stepVertical):
             promedios.append(calcularPromedio( (offsetHorizontal,offsetVertical), stepHorizontal, stepVertical, imagen))
     return  promedios
 
 
-archivoPromedios = open("promedios4D.txt", "w")
+archivoPromedios = open("promedios8D.txt", "w")
 for archivoImagen in os.listdir('./images'):
     promedios = procesarPromedioDeImagen(  Image.open("images/" + archivoImagen).convert('RGB'))
     archivoPromedios.write(archivoImagen)
